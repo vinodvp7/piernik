0f78532cf85d0390c44fa7f3502255bc8b4a3b2d
Artur Gawryszczak <gawrysz@gmail.com>
2023-07-10 22:26:54 +0200

    [particle_utils] is_part_in_cg now will work correctly for multicg and any(base_level%l%off < 0)

--- src/particles/particle_utils.F90
+++ src/particles/particle_utils.F90
@@ -47,7 +47,7 @@ contains
 
    subroutine is_part_in_cg(cg, pos, indomain, in, phy, out, fin)
 
-      use constants,     only: LO, HI, half, ndims, xdim, ydim, zdim
+      use constants,     only: LO, HI, ndims, xdim, ydim, zdim
       use domain,        only: dom
       use grid_cont,     only: grid_container
       use particle_func, only: particle_in_area, ijk_of_particle
@@ -58,18 +58,18 @@ contains
       real, dimension(ndims),        intent(in)  :: pos
       logical,                       intent(in)  :: indomain
       logical,                       intent(out) :: in, phy, out, fin
-      integer(kind=4), dimension(ndims)          :: ijk, lft, rgt
+      integer(kind=4), dimension(ndims)          :: ijk
 
       in  = particle_in_area(pos, cg%bnd_in)
       phy = particle_in_area(pos, cg%fbnd)
       out = particle_in_area(pos, cg%bnd_out)   ! Ghost particle
 
       ijk = ijk_of_particle(pos, dom%edge(:,LO), cg%idl)
-      lft = 0
-      rgt = ijk_of_particle(dom%edge(:,HI) + half * cg%dl, dom%edge(:,LO), cg%idl) ! do it better: find max indices for the domain in the current refinement level
-      ijk = max(ijk, lft)
-      ijk = min(ijk, rgt)
-      fin = cg%leafmap(ijk(xdim), ijk(ydim), ijk(zdim))
+      if (any(ijk(:) < cg%ijkse(:, LO) .and. dom%has_dir(:)) .or. any(ijk(:) > cg%ijkse(:, HI) .and. dom%has_dir(:))) then
+         fin = .false.
+      else
+         fin = cg%leafmap(ijk(xdim), ijk(ydim), ijk(zdim))
+      endif
 
       if (indomain) return
 

4c4c7d9cb93403247f7263d1b5b70782cef40fe7
npeschken <npeschken@camk.edu.pl>
2024-07-22 17:59:33 +0200

    [hydrostatic] Implementing new equilibrium for disc: thermal + hydrostatic

--- src/gravity/hydrostatic.F90
+++ src/gravity/hydrostatic.F90
@@ -35,6 +35,7 @@
 !<
 module hydrostatic
 ! pulled by GRAV
+   use constants, only: dsetnamelen
    use grid_cont, only: grid_container
 
    implicit none
@@ -42,22 +43,33 @@ module hydrostatic
    private
 
    public :: set_default_hsparams, hydrostatic_zeq_coldens, hydrostatic_zeq_densmid, cleanup_hydrostatic, outh_bnd, init_hydrostatic
+
+#ifdef THERM
+   public:: thermal_hydro_zeq_Tmid, Tprof, i_teq
+#endif /* THERM */
    public :: dprof, gprofs, nstot, zs, dzs, hsmin, hsbn, hsl, hscg
 
    real, allocatable, dimension(:) :: zs        !< array of z-positions of subgrid cells centers
    real, allocatable, dimension(:) :: gprofs    !< array of gravitational acceleration in a column of subgrid
    real, allocatable, dimension(:) :: dprofs
    real, allocatable, dimension(:) :: dprof     !< Array used for storing density during calculation of hydrostatic equilibrium
+   real, allocatable, dimension(:) :: Tprofs
+   real, allocatable, dimension(:) :: Tprof     !< Array used for storing Temperature during calculation of thermo-hydrostatic equilibrium
    real                            :: dzs       !< length of the subgrid cell in z-direction
    integer(kind=4)                 :: nstot     !< total number of subgrid cells in a column through all z-blocks
    integer                         :: rnsub     !< effective nsub relative to the refinement
    real                            :: dmid      !< density value in a midplane (fixed for hydrostatic_zeq_densmid, overwritten by hydrostatic_zeq_coldens)
+   real                            :: Tmid      !< Temperature value in a midplane
    real                            :: hsmin     !< lower position limit
    integer(kind=4),   dimension(2) :: hsbn      !< first and last cell indices in proceeded block
    real, allocatable, dimension(:) :: hsl       !< lower borders of cells of proceeded block
    type(grid_container), pointer   :: hscg
    logical                         :: unresolved = .false. !< check if grid subdivision is sufficient
    real                            :: urslvd               !< factor of grid subdivision insufficiency
+   logical                         :: chg_halo
+   character(len=dsetnamelen), parameter :: temp_eq = 'temperature_equilibrium'
+   integer                         :: i_teq, ksmin, ksmax
+   real                            :: dz!, zlim
 
    interface
       real function hzeqscheme(ksub, up)
@@ -76,9 +88,11 @@ contains
 !<
    subroutine init_hydrostatic
 
+      use cg_list_global,         only: all_cg
       use dataio_pub,            only: die
       use fluidboundaries_funcs, only: outh_fluidbnd
       use gravity,               only: get_gprofs, gprofs_target
+      use named_array_list,      only: qna
 
       implicit none
 
@@ -93,10 +107,15 @@ contains
                get_gprofs => get_gprofs_accel
             case ('extgp')
                get_gprofs => get_gprofs_extgp
+            case ('gpth')
+               get_gprofs => get_gprofs_extgp_th
             case default
                call die("[hydrostatic:init_hydrostatic] get_gprofs target has not been specified")
          end select
       endif
+
+      call all_cg%reg_var(temp_eq)
+      i_teq = qna%ind(temp_eq)
 
    end subroutine init_hydrostatic
 
@@ -171,9 +190,69 @@ contains
 
    end subroutine hydrostatic_zeq_densmid
 
+   !>
+!! \brief Routine that establishes hydrostatic + thermal equilibrium for a given midplane Temperature T0
+!! \details It is important to have get_gprofs pointer associated to a proper routine that gives back the column of nsub*nzt elements of gravitational acceleration in z direction. gprogs_target needs to be then set to "gpth"
+!! \param iia x-coordinate of z-column
+!! \param jja y-coordinate of z-column
+!<
+#ifdef THERM
+   subroutine thermal_hydro_zeq_Tmid(iia, jja, T0)
+
+      use constants,  only: half, small, two, LO, HI, zdim
+      use dataio_pub, only: die
+      use gravity,    only: get_gprofs
+
+      implicit none
+
+      integer,        intent(in)  :: iia, jja
+      real,           intent(in)  :: T0
+      integer                     :: ksub
+
+      if (T0 <= small) call die("[hydrostatic:thermal_hydro_zeq_Tmid] T0 must be /= 0")
+      Tmid = T0
+
+      allocate(zs(nstot))
+
+      do ksub = 1, nstot
+         zs(ksub) = hsmin + (real(ksub)-half) * dzs
+      enddo
+      ksmin = 1
+      do ksub = 1, nstot
+         if (zs(ksub) .ge. hscg%z(hscg%lhn(zdim, LO)) - dz/2) then
+            ksmin = ksub
+            exit
+         endif
+      enddo
+      ksmax = nstot
+      do ksub = 1, nstot
+         if (zs(ksub) .ge. hscg%z(hscg%lhn(zdim, HI)) + dz/2) then
+            ksmax = ksub - 1
+            exit
+         endif
+      enddo
+      allocate(gprofs(ksmin:ksmax), dprofs(ksmin:ksmax), Tprofs(ksmin:ksmax))
+      call get_gprofs(iia, jja)
+
+      if (any(abs(gprofs) >= two)) then
+         unresolved = .true.
+         urslvd = max(urslvd, maxval(abs(gprofs))/two)
+      endif
+
+      call thermal_hydro_main(iia,jja)
+
+      if (allocated(zs))     deallocate(zs)
+      if (allocated(gprofs)) deallocate(gprofs)
+      if (allocated(dprofs)) deallocate(dprofs)
+      if (allocated(Tprofs)) deallocate(Tprofs)
+
+    end subroutine thermal_hydro_zeq_Tmid
+
+#endif /* THERM */
+
 !>
 !! \brief Routine to set up sizes of arrays used in hydrostatic module. Settings depend on cg structure.
-!! \details Routine has to be called before the firs usage of hydrostatic_zeq_coldens/densmid if there is no other equivalent user settings.
+!! \details Routine has to be called before the first usage of thermal_hydro_zeq_Tmid if there is no other equivalent user settings.
 !<
    subroutine set_default_hsparams(cg)
 
@@ -189,20 +268,27 @@ contains
       real                                      :: mindz     !< cell size in z direction of the finest grid
 
       hscg => cg
-      mindz = dom%L_(zdim)/finest%level%l%n_d(zdim) ! if not is_defined then: mindz = cg%dl(zdim)
+      mindz = cg%dl(zdim)
 
-      nstot = nsub * int(finest%level%l%n_d(zdim) + 2*dom%nb, kind=4)  ! will fail silently somewhere beyond 20th refinement level
       dzs   = dom%L_(zdim)/(finest%level%l%n_d(zdim) * nsub)
-      !dzs   = mindz / nsub ! this simplification causes (different) truncation error
+      nstot = nsub * int(finest%level%l%n_d(zdim) + 2*dom%nb*mindz/dzs, kind=4)
       rnsub = nint(cg%dl(zdim) / dzs)
       hsmin = dom%edge(zdim, LO) - dom%nb * mindz
       hsbn  = cg%lhn(zdim,:)
       if (allocated(dprof)) deallocate(dprof)
+      if (allocated(Tprof)) deallocate(Tprof)
       allocate(dprof(hsbn(LO):hsbn(HI)))
+      allocate(Tprof(hsbn(LO):hsbn(HI)))
       if (allocated(hsl)) deallocate(hsl)
       allocate(hsl(hsbn(LO):hsbn(HI)+I_ONE))
       hsl(hsbn(LO):hsbn(HI)) = cg%coord(LEFT,  zdim)%r(hsbn(LO):hsbn(HI))
       hsl(hsbn(HI)+I_ONE)    = cg%coord(RIGHT, zdim)%r(hsbn(HI))
+
+      !zlim = 0
+      !dz = hsl(hsbn(HI)) - hsl(hsbn(HI)-1)
+      !do while (zlim .lt. 60) !650)
+      !   zlim = zlim + dz
+      !enddo
 
    end subroutine set_default_hsparams
 
@@ -258,6 +344,7 @@ contains
          do ksub = 1, nstot
             if (zs(ksub) > hsl(k) .and. zs(ksub) < hsl(k+1)) dprof(k) = dprof(k) + dprofs(ksub)/real(rnsub)
          enddo
+
       enddo
 
       if (present(sd)) then
@@ -267,7 +354,139 @@ contains
          enddo
       endif
 
-   end subroutine hydrostatic_main
+    end subroutine hydrostatic_main
+
+!>
+    !! \brief Routine that arranges thermal + hydrostatic equilibrium in the vertical (z) direction by setting the Temperatures and then the corresponding densities
+    !! \details This requires the current z array to either include the midplane, or to grab the boundaries values from the cg below / above
+!<
+#ifdef THERM
+   subroutine thermal_hydro_main(iia,jja)
+
+      use constants,  only: LO, HI, zdim
+      use dataio_pub, only: die
+      use mpisetup,    only: proc
+      use thermal,    only: find_temp_bin, alpha, Tref, lambda0, G1_heat, G0_heat
+      use units,      only: mH
+
+      implicit none
+
+      integer, intent(in)         :: iia, jja
+      integer                     :: ksub, ksmid, k, ii, i, iip, kstart
+      logical                     :: up, down, bnd_done
+
+
+      up = .false.
+      down = .false.
+      bnd_done = .false.
+      kstart = 1
+      Tprofs = 0.0
+      dprofs = 0.0
+      if (hscg%z(hscg%lh1(zdim, LO)) * hscg%z(hscg%lh1(zdim, HI)) .lt. 0.0) then
+         ksmid = 0
+         ksmid = minloc(abs(zs(ksmin:ksmax)),1) + ksmin -1
+         if (ksmid == 0) call die("[hydrostatic:thermal_hydro_main] ksmid not set")
+
+         if ((ksmid .lt. ksmin) .or. (ksmid .gt. ksmax)) print *, ksmin, ksmax, ksmid, zs(ksmin), zs(ksmax), zs(ksmid), minloc(abs(zs(ksmin:ksmax)),1)
+
+         Tprofs(ksmid) = Tmid
+         call find_temp_bin(Tmid, ii)
+         dprofs(ksmid) = G1_heat*mH / (lambda0(ii) * (Tmid/Tref(ii))**alpha(ii)*mH**2 - G0_heat*mH**2) * mH
+         if ((ksmid .ne. ksmin) .and. (ksmid .ne. ksmax)) then
+            Tprofs(ksmid+1) = Tmid
+            dprofs(ksmid+1) = G1_heat*mH / (lambda0(ii) * (Tmid/Tref(ii))**alpha(ii)*mH**2 - G0_heat*mH**2) * mH
+         endif
+         up = .true.
+         down = .true.
+         kstart = ksmid
+      else
+         if (hscg%z(hscg%ijkse(zdim, LO)) .gt. 0.0) then
+            up = .true.
+            kstart = ksmin - 1
+            if (hscg%q(i_teq)%arr(iia, jja, hscg%lh1(zdim, LO)) .eq. 0.0) then
+               Tprof = 100000
+               dprof = 0.0000001
+               !print *, 'T=0', proc, hscg%grid_id, hscg%x(iia), hscg%y(jja), hscg%z(hscg%lh1(zdim, HI))
+               return
+            endif
+            Tprofs(ksmin) = hscg%q(i_teq)%arr(iia, jja, hscg%lh1(zdim, LO))
+
+         else
+            down = .true.
+            kstart = ksmax
+            if (hscg%q(i_teq)%arr(iia, jja, hscg%lh1(zdim, HI)) .eq. 0.0) then
+               Tprof = 100000
+               dprof = 0.0000001
+               print *, 'T=0', proc, hscg%grid_id, hscg%x(iia), hscg%y(jja), hscg%z(hscg%lh1(zdim, HI))
+               return
+            endif
+            Tprofs(ksmax) = hscg%q(i_teq)%arr(iia, jja, hscg%lh1(zdim, HI))
+         endif
+      endif
+
+      !zlim = sqrt(hscg%x(iia)**2 + hscg%y(jja)**2)
+      !if ((kstart .lt. ksmin-1) .or. (kstart .gt. ksmax)) print *, 'Out of bounds', kstart, ksmin, ksmax !call die('[hydrostatics] Out of bounds')
+      if (up) then
+         i=0
+         chg_halo = .false.
+         !if (zs(kstart+1) .gt. zlim) chg_halo = .true.
+         if (Tprofs(kstart+1) .gt. 43287.612810830615) chg_halo = .true.
+         if (kstart < nstot) then
+            do ksub = kstart+1, ksmax-1
+               if ((ksub+1 .lt. ksmin) .or. (ksub+1 .gt. ksmax)) print *, 'Out of bounds: up', ksmin, ksmax, ksub+1
+               call find_temp_bin(Tprofs(ksub), ii)
+               Tprofs(ksub+1) = Tprofs(ksub) + Tzeq_scheme(ksub, 1.0, ii)
+               call find_temp_bin(Tprofs(ksub+1), ii)
+               dprofs(ksub+1) = G1_heat*mH /  (lambda0(ii) * (Tprofs(ksub+1)/Tref(ii))**alpha(ii)*mH**2 - G0_heat*mH**2) * mH
+               if (Tprofs(ksub+1) .gt. 1.0e10) then
+                  !print *, hscg%x(iia), hscg%y(jja),  zs(ksub+1), Tprofs(ksub+1), Tzeq_scheme(ksub, 1.0, ii, zlim), gprofs(ksub)
+                  call die('hydrostatic:thermal_hydro_main] Tprofs values problem')
+               endif
+               i=i+1
+            enddo
+         endif
+      endif
+
+      if (down) then
+         chg_halo = .false.
+         !if (zs(kstart) .lt. -1.0*zlim) chg_halo = .true.
+         if (Tprofs(kstart) .gt. 43287.612810830615) chg_halo = .true.
+         if (kstart > 1) then
+            do ksub = kstart, ksmin+1, -1
+               if ((ksub-1 .lt. ksmin) .or. (ksub-1 .gt. ksmax)) print *, 'Out of bounds: down', ksmin, ksmax, ksub+1
+               call find_temp_bin(Tprofs(ksub), ii)
+               Tprofs(ksub-1) = Tprofs(ksub) + Tzeq_scheme(ksub, -1.0, ii)
+               call find_temp_bin(Tprofs(ksub-1), ii)
+               dprofs(ksub-1) = G1_heat * mH /  (lambda0(ii) * (Tprofs(ksub-1)/Tref(ii))**alpha(ii)*mH**2 - G0_heat*mH**2) * mH
+               if (Tprofs(ksub-1) .gt. 1.0e10) then
+                  call die('hydrostatic:thermal_hydro_main] Tprofs values problem')
+               endif
+            enddo
+         endif
+      endif
+
+      Tprof(:) = 0.0
+      dprof(:) = 0.0
+      do k = hsbn(LO), hsbn(HI)
+         do ksub = ksmin, ksmax
+            if (zs(ksub) > hsl(k) .and. zs(ksub) < hsl(k+1)) then
+               Tprof(k) = Tprof(k) + Tprofs(ksub)/real(rnsub)
+               dprof(k) = dprof(k) + dprofs(ksub)/real(rnsub)
+            endif
+         enddo
+         if ((Tprof(k) .gt. 1.0e10) .or. (Tprof(k) .eq. 0.0)) then
+            print *, hscg%x(iia), hscg%y(jja), hscg%z(k), Tprof(k),  dprof(k), zs(ksmin), zs(ksmax)
+            call die('[hydrostatic:thermal_hydro_main] Problem with Temperature values')
+         endif
+      enddo
+      if ((sum(Tprof) .eq. 0.0) .or. (.not. (any(Tprof .gt. 0.0)))) call die('[hydrostatic:thermal_hydro_main] Tprof= 0')
+      if ((sum(dprof) .eq. 0.0) .or. (.not. (any(dprof .gt. 0.0)))) call die('[hydrostatic:thermal_hydro_main] dprof = 0')
+      hscg%q(i_teq)%arr(iia, jja, :) = 0.0
+      hscg%q(i_teq)%arr(iia, jja, :) = Tprof
+
+    end subroutine thermal_hydro_main
+
+#endif /* THERM */
 
    real function hzeq_scheme_v1(ksub, up) result(factor)
 
@@ -294,6 +513,59 @@ contains
    end function hzeq_scheme_v2
 #endif /* HYDROSTATIC_V2 */
 
+!>
+   !! \brief Computing the temperature of the next cell.
+   !! \details Once the temperature reach the unstable part of the cooling curve, an isobaric "jump" to a higher temperature is performed. This represents the transition to the halo.
+   !! \details Furthermore, an arbitrary factor factor_G0 is added to the equation, to ensure the disc is thick enough in a strong potential.
+!<
+#ifdef THERM
+   real function Tzeq_scheme(ksub, up, ii) result(factor)
+
+     use thermal,          only: G0_heat, alpha, lambda0, Tref, G1_heat, find_temp_bin
+     use units,            only: kboltz, mH
+
+      implicit none
+
+      integer, intent(in) :: ksub, ii
+      real,    intent(in) :: up
+      real                :: alpha_equi, factor_G0, lambda, lambda2, Press1, Press2, T ! alpha_equi to get from initproblem??
+      integer             :: jj
+
+      alpha_equi = 1
+      lambda = lambda0(ii) * (Tprofs(ksub)/Tref(ii))**alpha(ii)
+      factor_G0 = 1
+    !  if (chg_halo) then
+      factor_G0 = 2500
+      do while (lambda - G0_heat*factor_G0 .lt. 0)
+         factor_G0 = factor_G0 - 100
+      enddo
+  !    endif
+
+     ! chg_halo = .true.
+      !if ((abs(zs(ksub+nint(up))) .lt. zlim) .or.  (chg_halo)) then
+      if ((Tprofs(ksub) .lt. 43287.612810830615) .or. (chg_halo)) then
+         factor = - mH / (kboltz*(1+alpha_equi)) * gprofs(ksub) * (zs(ksub+nint(up)) - zs(ksub)) * (lambda*mH**2 - G0_heat*mH**2*factor_G0)/(lambda*(alpha(ii)-1)*mH**2 + G0_heat*mH**2*factor_G0)
+      else
+         Press1 = kboltz * G1_heat*mH / (lambda*mH**2 - G0_heat*mH**2) * Tprofs(ksub)
+         Press2 = 0
+         T = Tprofs(ksub)
+         do while (Press2 .lt. Press1)
+            T = T + 5000
+            call find_temp_bin(T, jj)
+            lambda2 = lambda0(jj) * (T/Tref(jj))**alpha(jj)
+            Press2 = kboltz * G1_heat*mH / (lambda2*mH**2 - G0_heat*mH**2) * T
+         enddo
+         factor = T - Tprofs(ksub)
+         chg_halo = .true.
+         if (abs(Press2 - Press1)/Press1 .gt. 100) print *, 'Warning! Discontinuity in Pressure', Press1, Press2, Tprofs(ksub), T
+      endif
+
+      factor=abs(factor)
+      if (Tprofs(ksub) + factor .lt. Tmid) factor = Tmid - Tprofs(ksub)
+
+    end function Tzeq_scheme
+#endif /* THERM */
+
    subroutine get_gprofs_accel(iia, jja)
 
       use constants, only: zdim
@@ -312,6 +584,37 @@ contains
 !! \brief Routine that has to offer a z-sweep of external gravity potential with extended z-grid
 !! \warning in case of moving 'use axes_M, only: axes'' behind use gravity there could be gcc(4.5) internal compiler error: in fold_convert_loc, at fold-const.c:2792 (solved in >=gcc-4.6)
 !<
+   subroutine get_gprofs_extgp_th(iia, jja)
+
+      use axes_M,    only: axes
+      use constants, only: half, ndims, zdim, LO, HI
+      use gravity,   only: tune_zeq, grav_type
+
+      implicit none
+
+      integer, intent(in)                     :: iia, jja
+      integer(kind=4), dimension(ndims,LO:HI) :: lhn
+      integer(kind=4)                         :: nstot1, i, j, k
+      real, dimension(:,:,:), pointer         :: gpots
+      type(axes)                              :: ax
+
+      allocate(gpots(1,1,ksmin:ksmax+1))
+      lhn = 1 ; lhn(zdim,LO) = ksmin ; lhn(zdim,HI) = ksmax+1
+      call ax%allocate_axes(lhn)
+      ax%x          = hscg%x(iia)
+      ax%y          = hscg%y(jja)
+      ax%z(ksmin:ksmax) = zs(ksmin:ksmax) - half*dzs
+      ax%z(ksmax+1)  = ax%z(ksmax) + dzs
+      call grav_type(gpots, ax, lhn)
+
+      gprofs(ksmin:ksmax) = (gpots(1,1,ksmin:ksmax) - gpots(1,1,ksmin+1:ksmax+1))/dzs
+      gprofs(:) = tune_zeq*gprofs(:)
+
+      call ax%deallocate_axes
+      if (associated(gpots)) deallocate(gpots)
+
+    end subroutine get_gprofs_extgp_th
+
    subroutine get_gprofs_extgp(iia, jja)
 
       use axes_M,    only: axes

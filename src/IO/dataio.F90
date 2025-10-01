a474303ab77b3061c3e980fd88b55c7253d8fb3d
Artur Gawryszczak <gawrysz@gmail.com>
2025-10-01 15:28:31 +0200

    [dataio] bugfix: don't ignore nrestart when restart/='last'

--- src/IO/dataio.F90
+++ src/IO/dataio.F90
@@ -241,6 +241,7 @@ contains
 #endif /* HDF5 */
       call piernik_MPI_Barrier
       call piernik_MPI_Bcast(nrestart)
+      restarted_sim = (nrestart > INVALID)
       call piernik_MPI_Bcast(restarted_sim)
 
       if (master) then
@@ -263,7 +264,7 @@ contains
    subroutine dataio_par_io
 
       use bcast,      only: piernik_MPI_Bcast
-      use constants,  only: idlen, cbuff_len, INT4, V_SILENT, V_DEBUG, V_VERBOSE, V_INFO, V_ESSENTIAL, V_WARN, v_name
+      use constants,  only: idlen, cbuff_len, INT4, V_SILENT, V_DEBUG, V_VERBOSE, V_INFO, V_ESSENTIAL, V_WARN, v_name, INVALID
       use dataio_pub, only: nres, nrestart, warn, nhdf, wd_rd, multiple_h5files, warn, h5_64bit, nh, set_colors, piernik_verbosity
       use mpisetup,   only: lbuff, ibuff, rbuff, cbuff, master, slave, nproc
 
@@ -274,7 +275,7 @@ contains
       restart       = 'last'   ! 'last': automatic choice of the last restart file regardless of "nrestart" value;
                               ! if something else is set: "nrestart" value is fixing
       res_id        = ''
-      nrestart      = 3
+      nrestart      = INVALID
 
       dt_hdf        = 0.0
       dt_res        = 0.0
@@ -929,9 +930,8 @@ contains
    subroutine find_last_restart(restart_number)
 
       use common_hdf5, only: output_fname
-      use constants,   only: RD
-      use dataio_pub,  only: restarted_sim, die
-      use constants,   only: INVALID
+      use constants,   only: RD, INVALID
+      use dataio_pub,  only: restarted_sim
 
       implicit none
 
@@ -940,8 +940,8 @@ contains
       integer(kind=4)              :: nres
       integer                      :: unlink_stat
       logical                      :: exist
-      if (restart_number == INVALID) then
-         restart_number = 0
+
+      restart_number = INVALID
 
          open(newunit=unlink_stat, file='restart_list.tmp', status='unknown')
          close(unlink_stat, status='delete')
